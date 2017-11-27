56c0e7c7c492b7f4f9b600c8dcc245ae52b0aadc
MetallicGloss <william@encryptedlaser.com>
2017-11-27 15:57:04 +0000

    Creation of location management windows.

---
+++ ELSM Project/manageLocationsCreate.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ELSM_Project
+{
+    partial class manageLocationsCreate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(manageLocationsCreate));
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnNewLocation = new System.Windows.Forms.Button();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.txtLocationName = new System.Windows.Forms.TextBox();
+            this.lblLatitude = new System.Windows.Forms.Label();
+            this.lblLongitude = new System.Windows.Forms.Label();
+            this.lblLocationName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(281, 145);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(207, 31);
+            this.btnCancel.TabIndex = 45;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnNewLocation
+            // 
+            this.btnNewLocation.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnNewLocation.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNewLocation.FlatAppearance.BorderSize = 0;
+            this.btnNewLocation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewLocation.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewLocation.Location = new System.Drawing.Point(36, 145);
+            this.btnNewLocation.Name = "btnNewLocation";
+            this.btnNewLocation.Size = new System.Drawing.Size(206, 31);
+            this.btnNewLocation.TabIndex = 44;
+            this.btnNewLocation.Text = "Process New Location";
+            this.btnNewLocation.UseVisualStyleBackColor = false;
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLatitude.Location = new System.Drawing.Point(178, 109);
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.Size = new System.Drawing.Size(310, 20);
+            this.txtLatitude.TabIndex = 43;
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLongitude.Location = new System.Drawing.Point(178, 71);
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.Size = new System.Drawing.Size(310, 20);
+            this.txtLongitude.TabIndex = 42;
+            // 
+            // txtLocationName
+            // 
+            this.txtLocationName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLocationName.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtLocationName.Location = new System.Drawing.Point(178, 33);
+            this.txtLocationName.Name = "txtLocationName";
+            this.txtLocationName.ReadOnly = true;
+            this.txtLocationName.Size = new System.Drawing.Size(310, 20);
+            this.txtLocationName.TabIndex = 41;
+            // 
+            // lblLatitude
+            // 
+            this.lblLatitude.AutoSize = true;
+            this.lblLatitude.BackColor = System.Drawing.Color.Transparent;
+            this.lblLatitude.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLatitude.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblLatitude.Location = new System.Drawing.Point(33, 110);
+            this.lblLatitude.Name = "lblLatitude";
+            this.lblLatitude.Size = new System.Drawing.Size(68, 18);
+            this.lblLatitude.TabIndex = 40;
+            this.lblLatitude.Text = "Latitude:";
+            // 
+            // lblLongitude
+            // 
+            this.lblLongitude.AutoSize = true;
+            this.lblLongitude.BackColor = System.Drawing.Color.Transparent;
+            this.lblLongitude.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLongitude.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblLongitude.Location = new System.Drawing.Point(33, 73);
+            this.lblLongitude.Name = "lblLongitude";
+            this.lblLongitude.Size = new System.Drawing.Size(82, 18);
+            this.lblLongitude.TabIndex = 39;
+            this.lblLongitude.Text = "Longitude:";
+            // 
+            // lblLocationName
+            // 
+            this.lblLocationName.AutoSize = true;
+            this.lblLocationName.BackColor = System.Drawing.Color.Transparent;
+            this.lblLocationName.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocationName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblLocationName.Location = new System.Drawing.Point(33, 34);
+            this.lblLocationName.Name = "lblLocationName";
+            this.lblLocationName.Size = new System.Drawing.Size(118, 18);
+            this.lblLocationName.TabIndex = 38;
+            this.lblLocationName.Text = "Location Name:";
+            // 
+            // manageLocationsCreate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ELSM_Project.Properties.Resources.imgBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(528, 203);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNewLocation);
+            this.Controls.Add(this.txtLatitude);
+            this.Controls.Add(this.txtLongitude);
+            this.Controls.Add(this.txtLocationName);
+            this.Controls.Add(this.lblLatitude);
+            this.Controls.Add(this.lblLongitude);
+            this.Controls.Add(this.lblLocationName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(544, 242);
+            this.MinimumSize = new System.Drawing.Size(544, 242);
+            this.Name = "manageLocationsCreate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create Location";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnNewLocation;
+        private System.Windows.Forms.TextBox txtLatitude;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.TextBox txtLocationName;
+        private System.Windows.Forms.Label lblLatitude;
+        private System.Windows.Forms.Label lblLongitude;
+        private System.Windows.Forms.Label lblLocationName;
+    }
+}
