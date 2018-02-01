29b4b302e7edc66c14d50b79b4cb4d8649bb98ff
William Phillips <metallicgloss@gmail.com>
2018-02-01 09:35:35 +0000

    Added cPanelSharp, updated dependencies and created setup window.

---
+++ ELSMProject/initialSetup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ELSM_Project
+{
+    public partial class initialSetup : Form
+    {
+        public initialSetup()
+        {
+            InitializeComponent();
+        }
+
+
+        private void initialSetup_Load(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("setup.xml");
+            string opt1 = doc.SelectSingleNode("Settings/Setup").InnerText;
+            System.Windows.Forms.MessageBox.Show(Convert.ToString(opt1));
+        }
+    }
+}
