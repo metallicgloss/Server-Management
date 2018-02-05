f30ddc1e59d75c693c6170cc555b65b1107b3acf
William Phillips <metallicgloss@gmail.com>
2018-02-05 15:08:51 +0000

    Re-addition of new ticket window.

---
+++ ELSMProject/newTicket.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ELSM_Project
+{
+    partial class newTicket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnNewTicket = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblRegarding = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.cmboRegarding = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(281, 323);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(207, 31);
+            this.btnCancel.TabIndex = 45;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnNewTicket
+            // 
+            this.btnNewTicket.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnNewTicket.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNewTicket.FlatAppearance.BorderSize = 0;
+            this.btnNewTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewTicket.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewTicket.Location = new System.Drawing.Point(36, 323);
+            this.btnNewTicket.Name = "btnNewTicket";
+            this.btnNewTicket.Size = new System.Drawing.Size(206, 31);
+            this.btnNewTicket.TabIndex = 44;
+            this.btnNewTicket.Text = "Create Ticket";
+            this.btnNewTicket.UseVisualStyleBackColor = false;
+            this.btnNewTicket.Click += new System.EventHandler(this.btnNewTicket_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Enabled = false;
+            this.txtEmail.Location = new System.Drawing.Point(178, 71);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(310, 20);
+            this.txtEmail.TabIndex = 42;
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(178, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(310, 20);
+            this.txtName.TabIndex = 41;
+            // 
+            // lblRegarding
+            // 
+            this.lblRegarding.AutoSize = true;
+            this.lblRegarding.BackColor = System.Drawing.Color.Transparent;
+            this.lblRegarding.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegarding.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblRegarding.Location = new System.Drawing.Point(33, 110);
+            this.lblRegarding.Name = "lblRegarding";
+            this.lblRegarding.Size = new System.Drawing.Size(82, 18);
+            this.lblRegarding.TabIndex = 40;
+            this.lblRegarding.Text = "Regarding:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblEmail.Location = new System.Drawing.Point(33, 73);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(85, 18);
+            this.lblEmail.TabIndex = 39;
+            this.lblEmail.Text = "Your Email:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblName.Location = new System.Drawing.Point(33, 34);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(90, 18);
+            this.lblName.TabIndex = 38;
+            this.lblName.Text = "Your Name:";
+            // 
+            // txtContent
+            // 
+            this.txtContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtContent.Location = new System.Drawing.Point(178, 145);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(310, 156);
+            this.txtContent.TabIndex = 47;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblMessage.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMessage.Location = new System.Drawing.Point(33, 146);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(73, 18);
+            this.lblMessage.TabIndex = 46;
+            this.lblMessage.Text = "Message:";
+            // 
+            // cmboRegarding
+            // 
+            this.cmboRegarding.FormattingEnabled = true;
+            this.cmboRegarding.Location = new System.Drawing.Point(178, 108);
+            this.cmboRegarding.Name = "cmboRegarding";
+            this.cmboRegarding.Size = new System.Drawing.Size(310, 21);
+            this.cmboRegarding.TabIndex = 48;
+            // 
+            // newTicket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ELSM_Project.Properties.Resources.imgBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(542, 385);
+            this.ControlBox = false;
+            this.Controls.Add(this.cmboRegarding);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNewTicket);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblRegarding);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(544, 242);
+            this.Name = "newTicket";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "0";
+            this.Load += new System.EventHandler(this.newTicket_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnNewTicket;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblRegarding;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ComboBox cmboRegarding;
+    }
+}
