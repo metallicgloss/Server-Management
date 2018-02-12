8d93c1b113700866fa5faeec91bc4132d1aec69e
William Phillips <metallicgloss@gmail.com>
2018-02-12 15:19:07 +0000

    Creation of user delete.

--- ELSMProject/userDelete.Designer.cs
+++ ELSMProject/userDelete.Designer.cs
@@ -28,11 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDeleteUser = new System.Windows.Forms.Button();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.cmboUserID = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(281, 84);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(207, 31);
+            this.btnCancel.TabIndex = 45;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDeleteUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDeleteUser.FlatAppearance.BorderSize = 0;
+            this.btnDeleteUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeleteUser.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteUser.Location = new System.Drawing.Point(36, 84);
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.Size = new System.Drawing.Size(206, 31);
+            this.btnDeleteUser.TabIndex = 44;
+            this.btnDeleteUser.Text = "Process User Deletion";
+            this.btnDeleteUser.UseVisualStyleBackColor = false;
+            this.btnDeleteUser.Click += new System.EventHandler(this.btnDeleteUser_Click);
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.BackColor = System.Drawing.Color.Transparent;
+            this.lblUserID.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblUserID.Location = new System.Drawing.Point(33, 34);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(62, 18);
+            this.lblUserID.TabIndex = 38;
+            this.lblUserID.Text = "User ID:";
+            // 
+            // cmboUserID
+            // 
+            this.cmboUserID.FormattingEnabled = true;
+            this.cmboUserID.Location = new System.Drawing.Point(178, 33);
+            this.cmboUserID.Name = "cmboUserID";
+            this.cmboUserID.Size = new System.Drawing.Size(310, 21);
+            this.cmboUserID.TabIndex = 46;
+            this.cmboUserID.SelectedIndexChanged += new System.EventHandler(this.cmboHostname_SelectedIndexChanged);
+            // 
+            // userDelete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "userDelete";
+            this.BackgroundImage = global::ELSM_Project.Properties.Resources.imgBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(528, 139);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDeleteUser);
+            this.Controls.Add(this.lblUserID);
+            this.Controls.Add(this.cmboUserID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(544, 178);
+            this.MinimumSize = new System.Drawing.Size(544, 178);
+            this.Name = "userDelete";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Delete User";
+            this.Load += new System.EventHandler(this.manageUsersDelete_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDeleteUser;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.ComboBox cmboUserID;
     }
 }
