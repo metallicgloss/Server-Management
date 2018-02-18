38942bf7afd733a7791216116e25a7703be894f6
William Phillips <metallicgloss@gmail.com>
2018-02-18 14:40:00 +0000

    Make ticket view page functional.

--- ELSMProject/ticketReply.Designer.cs
+++ ELSMProject/ticketReply.Designer.cs
@@ -28,11 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnPostReply = new System.Windows.Forms.Button();
+            this.pnlConfiguration = new System.Windows.Forms.Panel();
+            this.txtReply = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblPostReply = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(579, 594);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(541, 31);
+            this.btnCancel.TabIndex = 45;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnPostReply
+            // 
+            this.btnPostReply.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPostReply.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPostReply.FlatAppearance.BorderSize = 0;
+            this.btnPostReply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPostReply.Font = new System.Drawing.Font("Raleway", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPostReply.Location = new System.Drawing.Point(36, 594);
+            this.btnPostReply.Name = "btnPostReply";
+            this.btnPostReply.Size = new System.Drawing.Size(540, 31);
+            this.btnPostReply.TabIndex = 44;
+            this.btnPostReply.Text = "Submit Reply";
+            this.btnPostReply.UseVisualStyleBackColor = false;
+            this.btnPostReply.Click += new System.EventHandler(this.btnNewCommand_Click);
+            // 
+            // pnlConfiguration
+            // 
+            this.pnlConfiguration.AutoScroll = true;
+            this.pnlConfiguration.BackColor = System.Drawing.Color.Transparent;
+            this.pnlConfiguration.Location = new System.Drawing.Point(36, 31);
+            this.pnlConfiguration.Name = "pnlConfiguration";
+            this.pnlConfiguration.Size = new System.Drawing.Size(1084, 277);
+            this.pnlConfiguration.TabIndex = 50;
+            // 
+            // txtReply
+            // 
+            this.txtReply.Location = new System.Drawing.Point(36, 381);
+            this.txtReply.Multiline = true;
+            this.txtReply.Name = "txtReply";
+            this.txtReply.Size = new System.Drawing.Size(1084, 195);
+            this.txtReply.TabIndex = 52;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(164, 339);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(838, 1);
+            this.pictureBox1.TabIndex = 53;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblPostReply
+            // 
+            this.lblPostReply.AutoSize = true;
+            this.lblPostReply.BackColor = System.Drawing.Color.Transparent;
+            this.lblPostReply.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostReply.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPostReply.Location = new System.Drawing.Point(533, 329);
+            this.lblPostReply.Name = "lblPostReply";
+            this.lblPostReply.Size = new System.Drawing.Size(92, 19);
+            this.lblPostReply.TabIndex = 54;
+            this.lblPostReply.Text = "Post Reply";
+            // 
+            // ticketReply
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "newTicketReply";
+            this.AutoScroll = true;
+            this.BackgroundImage = global::ELSM_Project.Properties.Resources.imgBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1166, 649);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblPostReply);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtReply);
+            this.Controls.Add(this.pnlConfiguration);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnPostReply);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ticketReply";
+            this.Text = "Ticket Management";
+            this.Load += new System.EventHandler(this.serverControlEdit_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnPostReply;
+        private System.Windows.Forms.Panel pnlConfiguration;
+        private System.Windows.Forms.TextBox txtReply;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblPostReply;
     }
 }
--- ELSMProject/ticketReply.cs
+++ ELSMProject/ticketReply.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ELSM_Project
 {
@@ -15,6 +11,93 @@ namespace ELSM_Project
         public ticketReply()
         {
             InitializeComponent();
+        }
+
+        private static int loopnum, createloop, pointX = 235, pointY = 20, boxnum = 0, temploop;
+        private static bool finished, firstrun = true;
+        private static string value, yes = "No";
+        string[] userIDList = new string[100];
+        string[] replyContent = new string[100];
+
+        private void serverControlEdit_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connectionMySQL = new MySqlConnection(loginMenu.ConnectionString); // Open MySQL connection 
+            connectionMySQL.Open(); // Open MySQL Connection
+
+            MySqlCommand systemRepliesCMD = new MySqlCommand("SELECT * FROM systemReplies WHERE ticketID = @ticketID", connectionMySQL);
+            systemRepliesCMD.Parameters.AddWithValue("@ticketID", ticketView.ticketID);  // Replace string in query with variable
+            MySqlDataReader repliesReader = systemRepliesCMD.ExecuteReader(); // Execute MySQL reader query 
+
+            while (repliesReader.Read()) // While rows in reader
+            {
+                userIDList[loopnum] = Convert.ToString(repliesReader[2]); // Set array value to reader value - the command ID number
+                replyContent[loopnum] = Convert.ToString(repliesReader[3]); // Set array value to reader value - the command ID number
+                loopnum = loopnum + 1;
+            }
+            repliesReader.Close(); // Close reader
+
+            int totalloop = loopnum;
+            loopnum = 0;
+            
+            pnlConfiguration.Height += 40;
+            this.Height += 40;
+            while (loopnum != totalloop) // While rows in reader
+            {
+                MySqlCommand userDetailsCMD = new MySqlCommand("SELECT * FROM userAccounts WHERE userID = @userID", connectionMySQL);
+                userDetailsCMD.Parameters.AddWithValue("@userID", userIDList[loopnum]);  // Replace string in query with variable
+                MySqlDataReader userDetailReader = userDetailsCMD.ExecuteReader(); // Execute MySQL reader query 
+                userDetailReader.Read();
+                string forename = Convert.ToString(userDetailReader[3]);
+                string surname = Convert.ToString(userDetailReader[4]);
+                userDetailReader.Close();
+                Label box = new Label();
+                box.Name = "chkOS" + Convert.ToString(loopnum);
+                box.Text = forename + " " + surname;
+                box.AutoSize = true;
+                box.Location = new Point(10, loopnum * 20);
+                box.ForeColor = Color.White;
+                pnlConfiguration.Controls.Add(box);
+                loopnum += 1; // Add the value of 1 to the variable
+            }
+            int pointX = 235;
+            int pointY = 0;
+            int loopnum2 = 0; // Set variable to 0
+            for (int i = 0; i < loopnum; i++)  // Set variable to 0
+            {
+                Label a = new Label();
+                a.Location = new Point(pointX, pointY);
+                a.Name = "txtInput" + loopnum2;
+                a.Text = replyContent[loopnum2];
+                a.AutoSize = true;
+                a.ForeColor = Color.White;
+                pnlConfiguration.Controls.Add(a);
+                pnlConfiguration.Show();
+                pointY += 20;
+                loopnum2 += 1; // Add the value of 1 to the variable
+            }
+            this.Height += loopnum2 * 5;
+            pnlConfiguration.Height += (loopnum2 * 5);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Hide(); //Hide form
+        }
+       
+
+        private void btnNewCommand_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connectionMySQL = new MySqlConnection(loginMenu.ConnectionString); // Open MySQL connection 
+            connectionMySQL.Open();
+
+            MySqlCommand newTicketReply = new MySqlCommand("INSERT INTO systemReplies (ticketID, userID, replyContent) VALUES (@ticketID, @userID, @replyContent)", connectionMySQL);
+            newTicketReply.Parameters.AddWithValue("@ticketID", ticketView.ticketID);
+            newTicketReply.Parameters.AddWithValue("@userID", loginMenu.UserID);
+            newTicketReply.Parameters.AddWithValue("@replyContent", txtReply.Text);
+            newTicketReply.ExecuteNonQuery();
+
+
+            Hide();
         }
     }
 }
